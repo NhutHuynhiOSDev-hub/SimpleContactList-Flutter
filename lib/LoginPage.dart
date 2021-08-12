4c3cf0770dd8998eaa300471f4ab026a7623acc9
Nhut Huynh VeNtura <nhuthq@zigexn.vn>
2021-08-12 11:38:41 +0700

    add logo

---
+++ lib/LoginPage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'helper/Constants.dart';
+
+class LoginPage extends StatelessWidget {
+
+  final _pincodeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final logo = CircleAvatar(
+      backgroundColor: Colors.transparent,
+      radius: bigRadius,
+      child: appLogo,
+    );
+
+    final pinCode = TextFormField(
+      controller: _pincodeController,
+      keyboardType: TextInputType.phone,
+      maxLength: 4,
+      maxLines: 1,
+      decoration: InputDecoration(
+        hintText: pinCodeHintText,
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      style: TextStyle(
+        color: Colors.white
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {},
+        padding: EdgeInsets.all(12),
+        color: appDarkGreyColor,
+        child: Text(loginButtonText, style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: appDarkGreyColor,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: bigRadius),
+            pinCode,
+            SizedBox(height: buttonHeight),
+            loginButton
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/helper/Constants.dart
+++ lib/helper/Constants.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 // Colors
 Color appDarkGreyColor = Color.fromRGBO(58, 66, 86, 1.0);
+Color appGreyColor = Color.fromRGBO(64, 75, 96, .9);
 
 // Strings
 const appTitle = "Contact List";
+const pinCodeHintText = "Pin Code";
+const loginButtonText = "Login";
+
+// Images
+Image appLogo = Image.asset('assets/images/flutter-logo-round.png');
+
+// Sizes
+const bigRadius = 66.0;
+const buttonHeight = 24.0;
